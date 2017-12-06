8338089463327785e74135cee34abdd1baef45e2
Ray Deck <ray@raydeck.com>
2017-12-06 11:35:59 -0500

    Adding clarification that the podfile gets updated at react-native link

--- rnplugin/index.js
+++ rnplugin/index.js
@@ -2,6 +2,6 @@ module.exports = [
     {
         "name": 'installpods',
         "func": require("./installpods.js"),
-        "description": "Install all pods in the podfile"
+        "description": "Install all pods specified in the podfile. (Happens automatically at react-native link)"
     }
 ]
