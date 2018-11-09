f8b79d9bf18ce44f94cacc88e190b7beb4e3efee
Ray Deck <ray@raydeck.com>
2018-11-09 12:37:51 -0500

    Fixing issue #4  with refactoring installpods.js to just emulate the link functionality

--- rnplugin/installpods.js
+++ rnplugin/installpods.js
@@ -1,16 +1,14 @@
-const process = require('process');
-const fs = require('fs');
-var path = require('path'); 
-var {spawnSync} = require("child_process");
+const process = require("process");
+const fs = require("fs");
+var path = require("path");
+var { spawnSync } = require("child_process");
+const opts = {
+  encoding: "utf8",
+  stdio: "inherit"
+};
 module.exports = (config, args) => {
-    require(__dirname + "../makepods.js");
-    console.log("cwd", process.cwd());
-    var newpath = process.cwd() + "/ios";
-    process.chdir(newpath);
-    var args = ["install"];
-    const opts = {
-        'encoding': 'utf8', 
-        stdio: "inherit"
-    }
-    spawnSync("pod", args, opts);
-}
+  const makepodPath = path.join(__dirname, "..", "bin", "makepod.js");
+  spawnSync(makepodPath, [], opts);
+  const postlinkPath = path.join(__dirname, "..", "bin", "postlink.js");
+  spawnSync(postlinkPath, [], opts);
+};
