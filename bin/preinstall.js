13058b43bb2744abd8042760b6eab4197c8f9a06
Ray Deck <ray@raydeck.com>
2019-12-24 13:23:21 -0500

    Fixing preinstall to work in CI/CD where there might not be pods avail. (for Codepush envts)

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const execSync = require("child_process").execSync;
-out = execSync("which pod");
-if(!out) {
-    console.log("This package requires cocoapods to be installed on this machine. try running:\n\n\tgem install cocoapods\n\n")
-    exit(1); 
+const out = execSync("which pod");
+if (!out) {
+  console.warn(
+    "This package requires cocoapods to be installed on this machine. try running:\n\n\tgem install cocoapods\n\n"
+  );
+  // exit(1);
 }
