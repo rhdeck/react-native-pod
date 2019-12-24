0a3fbdce23b6640e0e528df942db47dbaa943199
Ray Deck <ray@raydeck.com>
2019-12-24 13:48:20 -0500

    Adding try/catch to preinstall

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 const execSync = require("child_process").execSync;
-const out = execSync("which pod");
-if (!out) {
-  console.warn(
-    "This package requires cocoapods to be installed on this machine. try running:\n\n\tgem install cocoapods\n\n"
-  );
-  // exit(1);
+try {
+  const out = execSync("which pod");
+  if (!out) {
+    console.warn(
+      "This package requires cocoapods to be installed on this machine. try running:\n\n\tgem install cocoapods\n\n"
+    );
+    // exit(1);
+  }
+} catch (e) {
+  console.warn("Could not run which test - possible CI/CD environment");
 }
